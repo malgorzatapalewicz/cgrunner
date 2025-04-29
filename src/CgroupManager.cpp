58744f20e5f69989d592a3459946830ac2fc92f2
Malgorzata Palewicz <malgorzata.maria.palewicz@gmail.com>
2025-04-29 13:23:38 +0200

    CgroupManager updated

--- src/CgroupManager.cpp
+++ src/CgroupManager.cpp
@@ -1,78 +1,51 @@
 #include "CgroupManager.h"
+#include "ErrorUtils.h"
+
 #include <iostream>
 #include <cstring>
 #include <sys/stat.h>
-#include <fstream>
 #include <unistd.h>
-
 
 namespace CgroupManager {
 
     const mode_t DIRECTORY_PERMISSIONS = 0755;
     const int CPU_PERIOD = 100000; //[ms]
+    const std::string CGROUP_BASE_PATH = "/sys/fs/cgroup/";
 
-    void reportError(const std::string& message, int errorCode){
-        std::cerr << message << ": " << strerror(errorCode) << std::endl;
+    std::string buildCgroupPath(const std::string& groupName, const std::string& file){
+        return CGROUP_BASE_PATH + groupName + (file.empty() ? "" : "/" + file);
     }
 
-  
     bool createGroup(const std::string& groupName){
-        std::string cgroupPath = "/sys/fs/cgroup/" + groupName;
+        std::string cgroupPath = buildCgroupPath(groupName);
             
         if(mkdir(cgroupPath.c_str(), DIRECTORY_PERMISSIONS) == -1){ 
-            reportError("Error creating cgroup", errno);
+            ErrorUtils::reportError("Error creating cgroup", errno);
             return false;
         }
         return true;
     }
 
     bool setCpuLimit(const std::string& groupName, int cpuPercent) {
-        std::string cpuLimitPath = "/sys/fs/cgroup/" + groupName + "/cpu.max";
-        std::ofstream file(cpuLimitPath);
-
-        if (!file.is_open()) {
-            reportError("Error opening cpu.max", errno);
-            return false;
-        }
-
-        file << cpuPercent * 1000 << " " << CPU_PERIOD;  
-        file.close();
-        return true;
+        std::string cpuLimitPath = buildCgroupPath(groupName, "cpu.max");
+        return writeToFile(cpuLimitPath, cpuPercent * 1000);
     }
 
     bool setMemoryLimit(const std::string& groupName, size_t memoryBytes) {
-        std::string memoryLimitPath = "/sys/fs/cgroup/" + groupName + "/memory.max";
-        std::ofstream file(memoryLimitPath);
-
-        if (!file.is_open()) {
-            reportError("Error opening memory.max", errno);
-            return false;
-        }
-
-        file << memoryBytes;
-        file.close();
-        return true;
+        std::string memoryLimitPath = buildCgroupPath(groupName, "memory.max");
+        return writeToFile(memoryLimitPath, memoryBytes);
     }
 
-    bool addProcessToCgroup(const std::string& groupName, pid_t pid) {
-        std::string cgroupProcsPath = "/sys/fs/cgroup/" + groupName + "/cgroup.procs";
-        std::ofstream file(cgroupProcsPath);
-
-        if (!file.is_open()) {
-            reportError("Error opening cgroup.procs", errno);
-            return false;
-        }
-
-        file << pid;  
-        file.close();
-        return true;
+    bool addProcessToCgroup(const std::string& groupName, pid_t pid){
+        std::string cgroupProcsPath = buildCgroupPath(groupName, "cgroup.procs");
+        return writeToFile(cgroupProcsPath, pid);
     }
     
     bool deleteCgroup(const std::string& groupName) {
-        std::string cgroupPath = "/sys/fs/cgroup/" + groupName;
+        std::string cgroupPath = buildCgroupPath(groupName);
 
         if (rmdir(cgroupPath.c_str()) == -1) {
-            reportError("Error removing cgroup", errno);
+            ErrorUtils::reportError("Error removing cgroup", errno);
             return false;
         }
 
